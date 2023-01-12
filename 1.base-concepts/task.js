a768c77bf5a0b781fd4209f25992db1bbca56164
Poryadin Maksim <maksim29skald@mail.ru>
2023-01-12 21:15:56 +0100

    second

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -9,7 +9,7 @@ function solveEquation(a, b, c) {
     arr[0] = (-b / (2 * a));
   } else if (d > 0) {
     arr[0] = (-b + Math.sqrt(d) ) / (2 * a);
-    arr[1] = (-b - Math.sqrt(d) ) / (2 * a); 
+    arr[1] = (-b - Math.sqrt(d) ) / (2 * a);  
   }
   // код для задачи №1 писать здесь
   return arr; // array
