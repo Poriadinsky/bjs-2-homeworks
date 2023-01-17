54c830dca76633851214425e1fd48efbce95994b
Poryadin Maksim <maksim29skald@mail.ru>
2023-01-17 10:52:27 +0100

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+   if (arr1.length !== arr2.length) {
+      return false;
+   }
+   return arr1.every((val, index) => val === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+   const filteredUsers = users.filter(user => user.gender === gender);
+   if (filteredUsers.length === 0) {
+      return 0;
+   }
+   const ages = filteredUsers.map(user => user.age);
+   const sumAges = ages.reduce((acc, age) => acc + age);
+   return sumAges / ages.length;
 }
