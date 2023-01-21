00024908caff41e94a5d88a0f9a19301a79f6398
Poryadin Maksim <maksim29skald@mail.ru>
2023-01-21 21:18:19 +0100

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+   this.name = name;
+   this.gender = gender;
+   this.age = age;
+   this.marks = [];
+   this.excluded = false;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+   this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+   if (this.excluded) {
+     throw new Error("Нельзя поставить оценку отчисленному студенту");
+   } else {
+     this.marks.push(...marksToAdd);
+   }
+ }
 
 Student.prototype.getAverage = function () {
-  
+   if (this.marks.length === 0) return 0;
+   return this.marks.reduce((sum, mark) => sum + mark) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+   this.excluded = reason;
+   delete this.marks;
+   delete this.subject;
 }
+
